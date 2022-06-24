9535f8b82cc29d7f2f00f44e7bd57b93420d7e1a
Toon Verwerft <toonverwerft@gmail.com>
2022-06-24 09:47:11 +0200

    Add example

--- examples/xx-php-soap.php
+++ examples/xx-php-soap.php
@@ -1,33 +1,26 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
 use Clue\React\Soap\Protocol\Psr18Browser;
 use Http\Client\Common\PluginClient;
 use Soap\Engine\SimpleEngine;
+use Soap\ExtSoapEngine\AbusedClient;
 use Soap\ExtSoapEngine\ExtSoapDriver;
 use Soap\ExtSoapEngine\ExtSoapOptions;
-use Soap\ExtSoapEngine\Wsdl\TemporaryWsdlLoaderProvider;
 use Soap\Psr18Transport\Psr18Transport;
-use Soap\Psr18Transport\Wsdl\Psr18Loader;
-use Soap\Wsdl\Loader\FlatteningLoader;
 use function React\Async\async;
 use function React\Async\await;
 use function React\Async\parallel;
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$browser = new React\Http\Browser();
-
-$blz = isset($argv[1]) ? $argv[1] : '12070000';
-
-
 $browser = new React\Http\Browser();
 $asyncHttpClient = new Psr18Browser($browser);
 $engine = new SimpleEngine(
-    ExtSoapDriver::createFromOptions(
-        ExtSoapOptions::defaults('http://www.thomas-bayer.com/axis2/services/BLZService?wsdl')
-            ->withWsdlProvider(new TemporaryWsdlLoaderProvider(
-                new FlatteningLoader(Psr18Loader::createForClient(new Psr18Browser($browser)))
-            ))
+    ExtSoapDriver::createFromClient(
+        $client = AbusedClient::createFromOptions(
+            ExtSoapOptions::defaults('http://www.dneonline.com/calculator.asmx?wsdl', [])
+                ->disableWsdlCache()
+        )
     ),
     $transport = Psr18Transport::createForClient(
         new PluginClient(
@@ -37,9 +30,12 @@ $engine = new SimpleEngine(
     )
 );
 
-$run = fn () => async(fn () => $engine->request('getBank', [array('blz' => $blz)]))()->then(
-    function ($result) {
-        echo 'SUCCESS!' . PHP_EOL;
+$add = async(function ($a, $b) use ($engine) {
+    return $engine->request('Add', [['intA' => $a, 'intB' => $b]]);
+});
+$addWithLogger = fn ($a, $b) => $add($a, $b)->then(
+    function ($result) use ($a, $b) {
+        echo "SUCCESS {$a}+{$b} = {$result->AddResult}!" . PHP_EOL;
         return $result;
     },
     function (Exception $e) {
@@ -48,9 +44,9 @@ $run = fn () => async(fn () => $engine->request('getBank', [array('blz' => $blz)
 );
 
 $results = await(parallel([
-    $run,
-    $run,
-    $run
+    fn() => $addWithLogger(1, 2),
+    fn() => $addWithLogger(3, 4),
+    fn() => $addWithLogger(5, 6)
 ]));
 
 var_dump($results);
